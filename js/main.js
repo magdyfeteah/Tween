bac38e0d66b4f27a57f7c2070c78afc62b823350
magdy <magdy.feteah@gmail.com>
2023-05-29 01:33:36 +0300

    change js code

--- js/main.js
+++ js/main.js
@@ -1,48 +1,33 @@
-function setActive(id) {
-    
 
-    var elements = document.getElementsByClassName("active");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.remove("active");
+// About Section
+// About Section Variables
+let buttons = document.querySelectorAll('.about .links button')
+let texts = document.querySelectorAll('.about .container .text')
+let images = document.querySelectorAll('.about .container .image')
+let links = document.querySelectorAll('.about .links button')
+
+// Switch About Section content
+buttons.forEach((button,id) =>{
+    button.onclick = ()=>{
+        // Switch Text
+        texts.forEach(text=>{
+            text.style.display='none'
+        })
+        texts[id].style.display='block'
+        // Switch Image
+        images.forEach(image=>{
+            image.style.display='none'
+        })
+        images[id].style.display='block'
+        // Switch class active on button
+        links.forEach(link=>{
+            link.classList.remove('active')
+        })
+        links[id].classList.add('active')
     }
-    document.getElementById(id).classList.add("active");
+})
 
-    var text1 = document.getElementById("text1");
-    var text2 = document.getElementById("text2");
-    var text3 = document.getElementById("text3");
-    var img1 = document.getElementById("img1");
-    var img2 = document.getElementById("img2");
-    var img3 = document.getElementById("img3");
-
-    if ( id == 1){
-        text1.style.display = "block";
-        text2.style.display = "none";
-        text3.style.display = "none";
-        img1.style.display = "block";
-        img2.style.display = "none";
-        img3.style.display = "none";
-    
-    }
-    else if ( id == 2){
-        text1.style.display = "none";
-        text2.style.display = "block";
-        text3.style.display = "none";
-        img1.style.display = "none";
-        img2.style.display = "block";
-        img3.style.display = "none";
-
-    }
-    else if ( id == 3){
-        text1.style.display = "none";
-        text2.style.display = "none";
-        text3.style.display = "block";
-        img1.style.display = "none";
-        img2.style.display = "none";
-        img3.style.display = "block";
-    }
-  
-}
-
+// Footer Section
 let year = document.querySelector(' .footer .copyright span:nth-child(1)')
 
 year.textContent=new Date().getFullYear()
